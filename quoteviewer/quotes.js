69f5026e247d4b386e42e81fa1d7811bd61afa9e
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2017-05-23 13:07:16 +0530

    Random quote generator of freecodecamp.

--- quoteviewer/quotes.js
+++ quoteviewer/quotes.js
@@ -3,12 +3,14 @@ var y = x.getElementsByTagName("p");
 var qtes = [{person : "Steve Jobs ",
              say : "\"Everyone should learn to programme cause it teaches us how to think.\""},
 {person : "Bill Gates", say : "\"Success is a lousy teacher. It seduces smart people into thinking they can't lose.\""},
- {person : "Mark Zuckerberg", say : "\"I think a simple rule of business is, if you do the things that are easier first, then you can actually make a lot of progress.\""}] ;
+ {person : "Mark Zuckerberg", say : "\"I think a simple rule of business is, if you do the things that are easier first, then you can actually make a lot of progress.\""},
+ {person : "The Shawsank Redemption", say : "\"Get busy living or get busy dying.\""},
+ {person : "Star Wars", say : "\"May the force be with you.\""}] ;
 y[0].innerHTML = qtes[0].say + " - " + 
   qtes[0].person;
 function generate()
 {
-  var temp = Math.floor(3 * Math.random());
+  var temp = Math.floor(5 * Math.random());
   y[0].innerHTML =  qtes[temp].say + " - " + 
   qtes[temp].person;
 }
