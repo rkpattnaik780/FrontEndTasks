058d4c3ef42f9a0fe2e75413c9e7a35c0fae5c53
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2017-06-02 18:31:59 +0530

    Simon game of freecodecamp

--- simonGame/gameplay.js
+++ simonGame/gameplay.js
@@ -2,15 +2,27 @@ $(document).ready(function(){
   $("#submit").hide() ;
   var simon = {
                 count : 1 ,
-                panels : [ "#redpanel" , "#greenpanel" , "#bluepanel" , "yelpanel" ] ,
+                panels : [ "#redpanel" , "#greenpanel" , "#bluepanel" , "#yelpanel" ] ,
                 playerMoves : [] ,
-                cpuMoves : [] ,
+                cpuMoves : [0,1,1,2,0,3,2] ,
                 tunes : [ new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3') ,
                           new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3') ,
                           new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3') ,
-                          new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3')] 
+                          new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3')] ,
+                color : ["red" , "green" , "blue" , "yellow"] ,
+                initcolor : ["#b30000" , "#004d00" , "#000099" , "#ffff99" ]
               };
-
+  function playSound(i)
+     {
+       if( i === 0)
+        { red.play() ;}
+       if( i === 1)
+        { green.play() ;}
+       if( i === 2)
+        { blue.play() ;}
+       if( i === 3)
+        { yellow.play() ;}
+     }
    var on = 0 ;
    
 
@@ -18,29 +30,38 @@ $(document).ready(function(){
        if( on === 0 )
          {
             on = 1 ;
+            showPattern( ) ;
          }
        else if ( on === 1)
          {   on = 0 ;
-             $("#countshow").html("0"); 
-             simon.count = 1;
+             $("#countshow").html("7"); 
+             simon.count = 0;
              simon.playerMoves = [] ;
              simon.cpuMoves = [] ; 
          }
-   });
-   function createPattern(){
-       for ( var i = 0 ; i < count ; i ++)
-         {
-            var temp = Math.floor(Math.random() * 4 );
-            simon.cpuMoves.push(temp);
-         }
-       
-    }
- 
-   
+   });   
    function showPattern(){
-       for(var j = 0 ; j < simon.cpuMoves.length ; j ++ )
-        {
-
-        }
+     for (var i = 0; i < simon.cpuMoves.length ; i++) { 
+    (function(i) {
+        setTimeout(function() { 
+          var temp = simon.panels[simon.cpuMoves[i]];
+          $(temp).css("background-color", simon.color[simon.cpuMoves[i]]);
+          playSound(simon.cpuMoves[i]) ;
+         // alert(temp) ;
+         }, i * 1000);
+         setTimeout(function() { 
+          var temp = simon.panels[simon.cpuMoves[i]];
+          $(temp).css("background-color", simon.initcolor[simon.cpuMoves[i]]);
+        //  alert(temp) ;
+         }, 300 + i * 1000);
+    })(i);
+}
+  $("#submit").show() ;
+  
    }
 }) ;
+/*for (var i = 1; i <= 3; i++) {
+    (function(index) {
+        setTimeout(function() { alert(index); }, i * 1000);
+    })(i);
+} */
